1efa13c7a544364c6d9347f8f7234d7450c3b9c2
Milagros Dominguez <dym.milagros.dominguez@gmail.com>
2026-02-24 13:33:25 +0000

    segundo commit

---
+++ addons/dev/custom_sale_codes/views/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Custom Sales Codes (Report)",
+    "version": "1.0.0",
+    "category": "Sales",
+    "summary": "Añade referencia interna y código de barras al reporte del pedido",
+    "depends": ["sales"],
+    "data": [
+        "views/sale_report.xml",
+        ],
+"installable": True,
+"aplication": False,
+"license": "LGPL-3"
+}
