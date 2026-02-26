c3bcc6bbc8fddda4da5b9dd379a6cb34277d1dcb
Milagros Dominguez <dym.milagros.dominguez@gmail.com>
2026-02-26 10:15:06 +0000

    actualizacion qa

---
+++ addons/qa/custom_sale_codes/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Custom Sales Codes (Report)",
+    "version": "1.0.0",
+    "category": "Sales",
+    "summary": "Añade referencia interna y código de barras al reporte del pedido",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_report.xml",
+        ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3"
+}
